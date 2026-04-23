b9c9c583fbe32805123f24629b438e3b8e5b08c3
sinandavood <sinandavood403@gmail.com>
2026-04-23 20:16:41 +0530

    first

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+//  ============  custom js start  ==========  //
+
+// aos animation
+
+AOS.init();
+$(document).on('ready', function () {
+  // initialization of aos
+  AOS.init({
+    duration: 2000,
+    once: true
+  });
+});
